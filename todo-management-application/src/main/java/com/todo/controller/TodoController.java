7c962d40199debd70f6afdc28e81ff6160da597c
JAYABABU8790 <141129946+JAYABABU8790@users.noreply.github.com>
2024-02-14 16:19:12 +0530

    Add files via upload

---
+++ todo-management-application/src/main/java/com/todo/controller/TodoController.java
@@ -0,0 +1,76 @@
+package com.todo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.todo.dto.TodoDto;
+import com.todo.service.TodoService;
+
+import lombok.AllArgsConstructor;
+
+@CrossOrigin("*")
+@AllArgsConstructor
+@RestController
+@RequestMapping("api/todos")
+public class TodoController {
+	
+	@Autowired
+	private TodoService todoService;
+	
+	@PostMapping
+	public ResponseEntity<TodoDto> addTodo(@RequestBody TodoDto todoDto){
+		TodoDto savedTodo=todoService.addTodo(todoDto);
+		return new ResponseEntity<>(savedTodo,HttpStatus.CREATED);	
+	}
+	
+	@GetMapping("{id}")
+	public ResponseEntity<TodoDto> getTodo(@PathVariable("id") Long todoId){
+		TodoDto todoDto=todoService.getTodo(todoId);
+		return new ResponseEntity<>(todoDto, HttpStatus.OK);	
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<TodoDto>> getAllTodos(){
+		List<TodoDto> todos=todoService.getAllTodos();
+		//return new ResponseEntity<>(todos,HttpStatus.OK);
+		return ResponseEntity.ok(todos);
+	}
+	
+	@PutMapping("{id}")
+	public ResponseEntity<TodoDto> updateTodo(@RequestBody TodoDto todoDto, @PathVariable("id") Long todoId){
+		TodoDto updatedTodo=todoService.updateTodo(todoDto, todoId);
+		return ResponseEntity.ok(updatedTodo);
+	}
+	
+	@DeleteMapping("{id}")
+	public ResponseEntity<String> deleteTodo(@PathVariable("id") Long todoId){
+		todoService.deleteTodo(todoId);
+		return ResponseEntity.ok("Todo deleted successfully..!");
+	}
+	
+	@PatchMapping("{id}/complete")
+	public ResponseEntity<TodoDto> completeTodo(@PathVariable("id") Long todoId){
+		TodoDto updatedTodo=todoService.completeTodo(todoId);
+		return ResponseEntity.ok(updatedTodo);
+	}
+	
+	@PatchMapping("{id}/in-complete")
+	public ResponseEntity<TodoDto> inCompleteTodo(@PathVariable("id") Long todoId){
+		TodoDto updatedTodo=todoService.inCompleteTodo(todoId);
+		return ResponseEntity.ok(updatedTodo);
+	}
+ 
+}
---
+++ todo-management-application/src/main/java/com/todo/entity/Todo.java
@@ -0,0 +1,57 @@
+package com.todo.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="todos")
+public class Todo {
+
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public boolean isCompleted() {
+		return completed;
+	}
+	public void setCompleted(boolean completed) {
+		this.completed = completed;
+	}
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable=false)
+	private String title;
+	
+	@Column(nullable = false)
+	private String description;
+	private boolean completed;
+}
